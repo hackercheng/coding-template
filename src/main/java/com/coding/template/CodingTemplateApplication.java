efb9715164891484783b983d816d58226cf91812
gecheng <tokencoder123@163.com>
2023-10-21 22:57:31 +0800

    fix: first init

---
+++ src/main/java/com/coding/template/CodingTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.coding.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coding/template/CodingTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
