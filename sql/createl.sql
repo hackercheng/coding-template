77d494f6535ca052a8d05e63d3521585ffb21a27
gecheng <tokencoder123@163.com>
2023-12-24 16:20:05 +0800

    feat: "增加了用户注销、查询个人详细信息功能"

--- sql/createl.sql
+++ sql/createl.sql
@@ -1,21 +1,23 @@
 -- 创建库
-create database if not exists code-template;
+create database if not exists code-db;
 
 -- 切换库
-use code-template;
+use code-db;
 
 -- 用户表
 create table if not exists user
 (
-    `id` bigint not null auto_increment comment '主键' primary key,
-    `createTime` datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    `updateTime` datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    `isDeleted` tinyint default 0 not null comment '是否删除(0-未删, 1-已删)',
-    `username` varchar(256)  comment '用户名',
-    `userAccount` varchar(256) not null comment '账户名',
-    `password` varchar(256) not null comment '密码',
-    `userAvatar` varchar(256)  comment '用户头像',
-    `phone` varchar(256)  comment '手机号',
-    `email` varchar(256)  comment '邮箱',
-    `userRole` tinyint default 0 not null comment '用户角色(0-管理员，1-用户)'
-    ) comment '用户表';
+    id           bigint auto_increment comment 'id' primary key,
+    userAccount  varchar(256)                           not null comment '账号',
+    userPassword varchar(512)                           not null comment '密码',
+    unionId      varchar(256)                           null comment '微信开放平台id',
+    mpOpenId     varchar(256)                           null comment '公众号openId',
+    userName     varchar(256)                           null comment '用户昵称',
+    userAvatar   varchar(1024)                          null comment '用户头像',
+    userProfile  varchar(512)                           null comment '用户简介',
+    userRole     varchar(256) default 'user'            not null comment '用户角色：user/admin/ban',
+    createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete     tinyint      default 0                 not null comment '是否删除',
+    index idx_unionId (unionId)
+    ) comment '用户' collate = utf8mb4_unicode_ci;
